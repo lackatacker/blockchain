7e41b7fc4c342085eac409cd2912ae84465eafe0
lackatacker <lackatacker@DESKTOP-N3V1CK4>
2021-12-18 09:56:01 +0100

    adding sha256 ecCreate ecToPub

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -4,7 +4,13 @@
 #include <stdint.h> /*for uint*/
 #include <stddef.h> /*contains NULL*/
 #include <openssl/sha.h>
+#include <openssl/ec.h> /*for eiptic curv creation*/
+#include <openssl/evp.h> /*contains secp256k1 curve*/
 #define SHA256_DIGEST_LENGTH 32
-
+#define EC_CURVE NID_secp256k1
+#define EC_PUB_LEN 65
 void _print_hex_buffer(uint8_t const *buf, size_t len);
+/* crypto tasks */
 uint8_t *sha256(int8_t const *s, size_t len,uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY *ec_create(void);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
